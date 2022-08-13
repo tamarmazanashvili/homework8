219d03996baf655840a5f5e09daab11b3a1418aa
Tamar Mazanashvili <109600480+tamarmazanashvili@users.noreply.github.com>
2022-08-14 00:02:42 +0400

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+let User = {
+    myName: "tamuna",
+    myLastName: "mazanashvili",
+    myAge: "22",
+    myMail: "tamar.mazanashvili.2@iliauni.edu.ge",
+    city: "tbilisi"
+
+}
+
+console.log(User.myName, User.myLastName, User.myAge, User.myMail, User.city)
